e5ab611d7ae51e3f166b5ef67fdda9a97d7273aa
Damon <damon.kenul@gmail.com>
2016-02-12 01:34:27 -0500

    add functionality to animate n-side polygon. unstable. need boundary checking function

--- assets/js/polygon-tile.js
+++ assets/js/polygon-tile.js
@@ -6,13 +6,18 @@ var PolygonTile = function(radius, polyColor, numSides) {
   var isRegular = numSides == 3 || numSides == 4 || numSides == 6;
   var tiltAmount = 0;
 
+  var roundBig = function(num) {
+    return +(Math.round(num + "e+12")  + "e-12");
+  };
+
   var createPolygon = function(point, offset) {
+
     offset = offset + tiltAmount || 0;
     var points = [];
 
     for (var i = 0; i < numSides; i++) {
-      var x = point[0] + radius * Math.sin(2*Math.PI*i/numSides + offset);
-      var y = point[1] - radius * Math.cos(2*Math.PI*i/numSides + offset);
+      var x = roundBig(point[0] + radius * Math.sin(2*Math.PI*i/numSides + offset));
+      var y = roundBig(point[1] - radius * Math.cos(2*Math.PI*i/numSides + offset));
 
       points.push(x + ',' + y);
     }
@@ -31,9 +36,30 @@ var PolygonTile = function(radius, polyColor, numSides) {
         .attr('stroke-width', 2)
         .attr('points', points)
         .attr("style", "fill-opacity:" + (Math.random() + 0.5) / 2)
-        .data({"center": point})
+        .datum({"center": point, "offset": offset});
     }
 
+  };
+
+  var appendRegular = function(point, offset) {
+    debugger;
+
+    offset = offset + tiltAmount || 0;
+    var halfAngle = (Math.PI / numSides);
+    var centerToSide = radius*Math.cos(halfAngle);
+    offset += halfAngle;
+
+
+    for (var i = 0; i < numSides; i++) {
+      var x = roundBig(point[0] + 2*centerToSide * Math.sin(2*Math.PI*i/numSides + offset));
+      var y = roundBig(point[1] - 2*centerToSide * Math.cos(2*Math.PI*i/numSides + offset));
+
+      if (numSides % 2 == 0) {
+        createPolygon([x,y], 0);
+      } else {
+        createPolygon([x,y], 2*Math.PI*i/numSides + offset);
+      }
+    }
   };
 
 
@@ -95,8 +121,8 @@ var PolygonTile = function(radius, polyColor, numSides) {
   this.animate = function() {
     var counter = 0;
     var animationInterval = setInterval(function() {
-      startX = ($(window).width() - radius) / 2
-      startY = ($(window).height() - radius) / 2
+      startX = (Math.max(document.documentElement.clientWidth, window.innerWidth || 0) - radius) / 2
+      startY = (Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - radius) / 2
       if (counter == 0) {
         d3.select('body')
           .append('svg')
@@ -107,10 +133,11 @@ var PolygonTile = function(radius, polyColor, numSides) {
       }
 
 
-      // $('polygon').each(function() {
-      //   var points = $( this ).attr('points');
-      //   createNewSquares(points);
-      // })
+      d3.selectAll('polygon').each(function() {
+        var center = d3.select(this).datum().center;
+        var offset = d3.select(this).datum().offset;
+        appendRegular(center, offset);
+      })
 
       counter++;
 
@@ -120,32 +147,32 @@ var PolygonTile = function(radius, polyColor, numSides) {
     }, 150)
   };
 
-  this.render = function() {
-    var counter = 0;
-
-    while (1) {
-      startX = ($(window).width() - radius) / 2
-      startY = ($(window).height() - radius) / 2
-      if (counter == 0) {
-        d3.select('body')
-          .append('svg')
-          .attr('width', '100%')
-          .attr('height', '100%');
-
-        createPolygon([startX, startY]);
-      }
-
-
-      $('polygon').each(function() {
-        var points = $( this ).attr('points');
-        createNewSquares(points);
-      })
-
-      counter++;
-
-      if (counter == 40) {
-        break;
-      }
-    }
-  }
+  // this.render = function() {
+  //   var counter = 0;
+  //
+  //   while (1) {
+  //     startX = ($(window).width() - radius) / 2
+  //     startY = ($(window).height() - radius) / 2
+  //     if (counter == 0) {
+  //       d3.select('body')
+  //         .append('svg')
+  //         .attr('width', '100%')
+  //         .attr('height', '100%');
+  //
+  //       createPolygon([startX, startY]);
+  //     }
+  //
+  //
+  //     // $('polygon').each(function() {
+  //     //   var points = $( this ).attr('points');
+  //     //   createNewSquares(points);
+  //     // })
+  //
+  //     counter++;
+  //
+  //     if (counter == 40) {
+  //       break;
+  //     }
+  //   }
+  // };
 }

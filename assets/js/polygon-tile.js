57453c8932fc0b9a70ea4329e570a771493f312d
Damon <damon.kenul@gmail.com>
2016-02-11 22:08:31 -0500

    begin to abstract square into any sided polygon

--- assets/js/polygon-tile.js
+++ assets/js/polygon-tile.js
@@ -1,7 +1,27 @@
-var PolygonTile = function(polySize, polyColor) {
+var PolygonTile = function(radius, polyColor, numSides) {
 
-  var polySize = polySize || 100;
+  var radius = radius || 50;
   var polyColor = polyColor || 'blue';
+  var numSides = numSides || 4;
+  var isRegular = numSides == 3 || numSides == 4 || numSides == 6;
+
+  var createRegularPolygon = function(point, offset) {
+    offset = offset || 0;
+    var points = [];
+
+    for (var i = 1; i <= numSides; i++) {
+      var x = point[0] + Math.cos(2*Math.pi*i/numSides + offset);
+      var y = point[1] + Math.sin(2*Math.pi*i/numSides + offset);
+
+      points.push(x + ',' + y);
+    }
+
+    points = points.join(' ');
+
+
+
+  };
+
 
   var parsePoints = function(points) {
     var result = [];
@@ -20,11 +40,11 @@ var PolygonTile = function(polySize, polyColor) {
   var createNewSquare = function(point) {
     var x = point[0];
     var y = point[1];
-    var newPoints = x+','+y+' '+x+','+(y+polySize)+' '+(x+polySize)+','+(y+polySize)+' '+(x+polySize)+','+y;
+    var newPoints = x+','+y+' '+x+','+(y+radius)+' '+(x+radius)+','+(y+radius)+' '+(x+radius)+','+y;
 
     if (!$('polygon[points="' + newPoints + '"]')[0] &&
-        !(x < -(polySize) || x > $(window).width()) &&
-        !(y < -(polySize) || y > $(window).height())) {
+        !(x < -(radius) || x > $(window).width()) &&
+        !(y < -(radius) || y > $(window).height())) {
 
       d3.select('svg')
         .append('polygon')
@@ -41,8 +61,8 @@ var PolygonTile = function(polySize, polyColor) {
   var createNewSquares = function(points) {
     var parsedPoints = parsePoints(points);
 
-    var left = [parsedPoints[0][0] - polySize, parsedPoints[0][1]];
-    var top = [parsedPoints[0][0], parsedPoints[0][1] - polySize];
+    var left = [parsedPoints[0][0] - radius, parsedPoints[0][1]];
+    var top = [parsedPoints[0][0], parsedPoints[0][1] - radius];
     var bottom= parsedPoints[1];
     var right = parsedPoints[3];
 
@@ -52,11 +72,11 @@ var PolygonTile = function(polySize, polyColor) {
     createNewSquare(right);
   };
 
-  this.startAnimation = function() {
+  this.animate = function() {
     var counter = 0;
     var animationInterval = setInterval(function() {
-      startX = ($(window).width() - polySize) / 2
-      startY = ($(window).height() - polySize) / 2
+      startX = ($(window).width() - radius) / 2
+      startY = ($(window).height() - radius) / 2
       if (counter == 0) {
         d3.select('body')
           .append('svg')
@@ -79,4 +99,33 @@ var PolygonTile = function(polySize, polyColor) {
       }
     }, 150)
   };
+
+  this.render = function() {
+    var counter = 0;
+
+    while (1) {
+      startX = ($(window).width() - radius) / 2
+      startY = ($(window).height() - radius) / 2
+      if (counter == 0) {
+        d3.select('body')
+          .append('svg')
+          .attr('width', '100%')
+          .attr('height', '100%');
+
+        createNewSquare([startX, startY]);
+      }
+
+
+      $('polygon').each(function() {
+        var points = $( this ).attr('points');
+        createNewSquares(points);
+      })
+
+      counter++;
+
+      if (counter == 40) {
+        break;
+      }
+    }
+  }
 }

23be36252e3bea2b0a32fe4b9b9bf1ecc460ccff
Damon <damon.kenul@gmail.com>
2016-02-12 12:27:04 -0500

    move deprecated code

--- assets/js/polygon-tile.js
+++ assets/js/polygon-tile.js
@@ -6,10 +6,6 @@ var PolygonTile = function(radius, polyColor, numSides) {
   var isRegular = numSides == 3 || numSides == 4 || numSides == 6;
   var tiltAmount = 0;
 
-  var roundBig = function(num) {
-    return +(Math.round(num + "e+12")  + "e-12");
-  };
-
   var createPolygon = function(point, offset, iteration) {
     iteration = iteration || 0;
     offset = offset + tiltAmount || 0;
@@ -58,56 +54,6 @@ var PolygonTile = function(radius, polyColor, numSides) {
         createPolygon([x,y], 2*Math.PI*i/numSides + offset, iteration);
       }
     }
-  };
-
-
-  var parsePoints = function(points) {
-    var result = [];
-    points.split(' ').forEach(function(point) {
-      var intPair = [];
-      point.split(',').forEach(function(num){
-        intPair.push(parseFloat(num));
-      });
-
-      result.push(intPair);
-    });
-
-    return result;
-  };
-
-  var createNewSquare = function(point) {
-    var x = point[0];
-    var y = point[1];
-    var newPoints = x+','+y+' '+x+','+(y+radius)+' '+(x+radius)+','+(y+radius)+' '+(x+radius)+','+y;
-
-    if (!$('polygon[points="' + newPoints + '"]')[0] &&
-        !(x < -(radius) || x > $(window).width()) &&
-        !(y < -(radius) || y > $(window).height())) {
-
-      d3.select('svg')
-        .append('polygon')
-        .attr('class', 'new-poly')
-        .attr('fill', polyColor)
-        .attr('stroke', polyColor)
-        .attr('stroke-width', 2)
-        .attr('points', newPoints)
-        .attr("style", "fill-opacity:" + (Math.random() + 0.5) / 2);
-
-    }
-  };
-
-  var createNewSquares = function(points) {
-    var parsedPoints = parsePoints(points);
-
-    var left = [parsedPoints[0][0] - radius, parsedPoints[0][1]];
-    var top = [parsedPoints[0][0], parsedPoints[0][1] - radius];
-    var bottom= parsedPoints[1];
-    var right = parsedPoints[3];
-
-    createNewSquare(left);
-    createNewSquare(top);
-    createNewSquare(bottom);
-    createNewSquare(right);
   };
 
   this.tilt = function() {
